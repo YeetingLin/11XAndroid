ef06f3f44e947ec12a222841ebda1710d878bba5
YeetingLin <a110223044@mail.shu.edu.tw>
2024-03-22 14:59:26 +0800

    2024/03/22

---
+++ UI_Test/app/src/main/java/com/example/ui_test/DisplayStringActivity.java
@@ -0,0 +1,23 @@
+package com.example.ui_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayStringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_display_string);
+
+
+        TextView txvShow = (TextView) findViewById(R.id.txvShow);
+        Intent intent =getIntent();
+        String reveivddString = intent.getStringExtra("userInput"); //接收字串資料name後需要一樣
+        txvShow.setText("輸入:"+intent.getStringExtra("userInput"));
+    }
+
+}
