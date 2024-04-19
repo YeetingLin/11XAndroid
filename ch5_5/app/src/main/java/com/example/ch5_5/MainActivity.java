4a6f4b787d789e49d86470963d4155f5847f0442
YeetingLin <a110223044@mail.shu.edu.tw>
2024-04-19 14:44:31 +0800

    2024-04-19

---
+++ ch5_5/app/src/main/java/com/example/ch5_5/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ch5_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
