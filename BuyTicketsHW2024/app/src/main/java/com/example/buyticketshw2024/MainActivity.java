f5cb7480b550b5df4e34916e3fc4975ac2cc5833
YeetingLin <a110223044@mail.shu.edu.tw>
2024-05-03 14:56:03 +0800

    113-05-03

---
+++ BuyTicketsHW2024/app/src/main/java/com/example/buyticketshw2024/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.buyticketshw2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
